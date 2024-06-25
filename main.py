d51c04121ed84d58af3cdd99c0516fcec7947839
Sam <54629166+saippu@users.noreply.github.com>
2024-06-25 13:49:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import numpy as np
 import subprocess
 import shlex
 import webcolors
+import bisect
 latest = [0]
 colour2={}
 def resizing(file, name, value):
@@ -16,37 +17,64 @@ def resizing(file, name, value):
     sizing.save(name, dpi=(round(img.width/value), round(img.height/value)))
 
 def colour_and_brightness(pic):
+    start = time.time()
     img = Image.open(pic)
     data = img.getdata()
     for i in data:
         bright = (i[0]*299+i[1]*587+i[2]*144) / 1000
         color = i[0],i[1],i[2]
-        colour2.update({round(bright): color})
+        colour2.update({int(round(bright)): color})
+    print(time.time()-start)
+
 
 def main():
-        start = time.time()
-        latest[0] = plugs.song()
-        res = requests.get(plugs.artwork(), stream=True)
-        with open("somesong.png", 'wb') as f:
-            shutil.copyfileobj(res.raw,f)
-        resizing("somesong.png", "somesong.png", 8) #7 or 6
-        colour_and_brightness("somesong.png")
-        print(len(colour2))
-        img = Image.open("sun.png")
-        data = img.getdata()
-        ndata = []
-        for i in data:
-            brightt = (i[0]*299+i[1]*587+i[2]*144) / 1000
-            closest = colour2.get(brightt) or colour2[min(colour2.keys(),key=lambda key: abs(key-brightt))]
-            ndata.append(closest)
-        img.putdata(ndata)
-        img.save("wallpapermodpil.png", "PNG")
-        img = Image.open("wallpapermodpil.png")
-        subprocess.Popen(shlex.split(f'swww img ~/spotifywall/wallpapermodpil.png'), stdout=subprocess.PIPE)
-        print(time.time()-start)
+        open("colors.txt", "w").close()
+        if plugs.artwork() == None:
+            pass
+        elif plugs.artwork() == '':
+            pass
+        else:
+            start = time.time()
+            latest[0] = plugs.song()
+            res = requests.get(plugs.artwork(), stream=True)
+            with open("somesong.png", 'wb') as f:
+                shutil.copyfileobj(res.raw,f)
+            resizing("somesong.png", "somesong.png", 5) #7 or 6
+            colour_and_brightness("somesong.png")
+            print(len(colour2))
+            img = Image.open("sun.png")
+            data = img.getdata()
+            ndata = []
+            colour = sorted(list(colour2.keys()))
+            for i in data:
+                brightt = (i[0]*299+i[1]*587+i[2]*144) / 1000
+                if brightt in colour2:
+                    search = colour2.get(brightt)
+                    ndata.append(search)
+                else:
+                    bi = bisect.bisect_right(colour, brightt)
+                    if bi ==0:
+                       search = colour2[colour[0]]
+                       ndata.append(search)
+                    else:
+                       search = colour2[colour[bi-1]]
+                       ndata.append(search)
+                """if brightt in colour2:
+                    ndata.append(colour2[brightt])
+                else:
+                    search = bisect.bisect_left(colour, brightt)
+                    if search == 0:
+                        ndata.append(colour2[colour[0]])
+                    else:
+                        ndata.append(colour2[colour[search]])"""
+            img.putdata(ndata)
+            img.save("wallpapermodpil.png", "PNG")
+            img = Image.open("wallpapermodpil.png")
+            subprocess.Popen(shlex.split(f'swww img /home/Sam/spotifywall/wallpapermodpil.png'), stdout=subprocess.PIPE)
+            print(time.time()-start)
 
 while True:
-    if plugs.song() == None:
+    if plugs.artwork() == None:
         pass
     if latest[0]==0:
         main()
