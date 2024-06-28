fa62b90bd098f24a2670720be9ba72c0930aaf08
Sam <54629166+saippu@users.noreply.github.com>
2024-06-28 09:45:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ import subprocess
 import shlex
 import webcolors
 import bisect
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--image', default=1,required=True,dest ="image")
+args = vars(parser.parse_args())
+args = args["image"]
 latest = [0]
 colour2={}
 def resizing(file, name, value):
     img = Image.open(file)
     sizing = img.resize((round(img.width/int(value)),round(img.height/int(value))), resample=Image.Resampling.BILINEAR)
     sizing.save(name, dpi=(round(img.width/value), round(img.height/value)))
-
 def colour_and_brightness(pic):
     start = time.time()
     img = Image.open(pic).convert("RGB")
@@ -25,9 +29,7 @@ def colour_and_brightness(pic):
         color = i[0],i[1],i[2]
         colour2.update({int(round(bright)): color})
     print(time.time()-start)
-
-
-def main():
+def main(arg1):
         if plugs.artwork() == None:
             pass
         elif plugs.artwork() == '':
@@ -41,7 +43,7 @@ def main():
             resizing("somesong.png", "somesong.png", 5) #7 or 6
             colour_and_brightness("somesong.png")
             print(len(colour2))
-            img = Image.open("sun.png")
+            img = Image.open(arg1)
             data = img.getdata()
             ndata = []
             colour = sorted(list(colour2.keys()))
@@ -58,20 +60,28 @@ def main():
                     else:
                        search = colour2[colour[bi-1]]
                        ndata.append(search)
+                """if brightt in colour2:
+                    ndata.append(colour2[brightt])
+                else:
+                    search = bisect.bisect_left(colour, brightt)
+                    if search == 0:
+                        ndata.append(colour2[colour[0]])
+                    else:
+                        ndata.append(colour2[colour[search]])"""
             img.putdata(ndata)
             img.save("wallpapermodpil.png", "PNG")
             img = Image.open("wallpapermodpil.png")
-            subprocess.Popen(shlex.split(f'swww img /home/Sam/spotifywall/wallpapermodpil.png'), stdout=subprocess.PIPE)
+            subprocess.Popen(shlex.split(f'swww img ~/spotifywall/wallpapermodpil.png'), stdout=subprocess.PIPE)
             print(time.time()-start)
 
 while True:
     if plugs.artwork() == None:
         pass
     if latest[0]==0:
-        main()
+        main(args)
     if latest[0]==plugs.song():
         pass
     else:
         latest[0]=0
         colour2.clear()
-        main()
+        main(args)
